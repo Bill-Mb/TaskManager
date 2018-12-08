266ef83296f78d45ca7eb5cd8c3c96cced2030ba
william <mbouna.william@athena.cm>
2018-12-08 12:20:17 +0100

    project initialization

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button as NativeButton } from 'react-native';
+import { Button, ThemeProvider } from 'react-native-elements';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app! Tata</Text>
+          <Button title="Hey!" />
+          <NativeButton title="Hey!" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
