c40d8638bf81bc6e2a429ec41ea76f3b602e62cf
will <mbouna.william@athena.cm>
2019-06-09 14:09:36 +0100

    add @material-ui

--- src/RootLayout/translations.js
+++ src/RootLayout/translations.js
@@ -5,6 +5,6 @@ export const moduleName = "TaskManager.RootLayout";
 export default defineMessages({
   welcome: {
     id: `${moduleName}.welcome`,
-    defaultMessage: "Open up App.js to start working on your app! Tata",
+    defaultMessage: "Open up App.js to start working on your app! Bill Mb.",
   },
 });
